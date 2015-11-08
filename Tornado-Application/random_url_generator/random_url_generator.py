832aa41d13ef56fc7f79bdec6a03f331bc993211
mxlei01 <mxlei01@gmail.com>
2015-11-08 01:23:13 -0700

    Added a random_url_generator that basically generates a random 6 digit (or any length in the random_url_settings.py file

---
+++ Tornado-Application/random_url_generator/random_url_generator.py
@@ -0,0 +1,68 @@
+import random_url_settings
+import random
+import urlparse
+from tornado import gen
+from executor_thread_pool.executor import executor
+from logger.logger import logger
+
+class AsyncRandomURLGenerator(object):
+    # Usage:
+    #       This class is responsible for generating a string of letters and digits randomly with a specified
+    #       base, for example base 16 will include both numbers(0-9) and letters(A-F), and combine it
+    #       with a domain_base.
+    #       For example, if the domain base is http://mxlei01.com/url-shortener, and the generated
+    #       shorted URL digits is 13EF0, then this class will output http://mxlei01.com/url-shortener/13ER90
+
+    def __init__(self,
+                 random_base=random_url_settings.random_base,
+                 domain_base=random_url_settings.domain_base,
+                 length_url=random_url_settings.length_url,
+                 logger=logger,
+                 executor=executor):
+        # Usage:
+        #       constructor for AsyncRandomURLGenerator
+        # Arguments:
+        #       random_base (array)  : array of numbers/letters
+        #       domain_base (string) : a domain that we will attach our generated numbers to
+        #       length_url  (int)    : a integer specifying the length of the shortened URL
+        #       logger      (object) : a logger object
+        #       executor    (object) : a executor object that allows to execute functions asynchronously
+        # Return:
+        #       None
+
+        self.random_base = random_base
+        self.domain_base = domain_base
+        self.length_url = length_url
+        self.logger = logger
+        self.executor = executor
+
+    @gen.coroutine
+    def generate_url(self):
+        # Usage:
+        #       Function will use an executor to create a random path
+        #       Then attaching it with domain_base using urlparse library.
+        # Arguments:
+        #       None
+        # Return:
+        #       URL (future) : a future containing the result of the url shortening
+
+        # The path variable now contains a randomized string of self.length_url length
+        path = yield self.executor.submit(self.generate_random_path)
+
+        # Use the urlparse library to join the domain_base and the path to form a url
+        url = urlparse.urljoin(self.domain_base, path)
+        self.logger.info("Generated a URL:%s" % url)
+
+        # Return a future containing the data of
+        raise gen.Return(url)
+
+    def generate_random_path(self):
+        # Usage:
+        #       Function when called with utilize self.random_base to generate a string of
+        #       letters or numbers. The length of the string will be determined by the self.length_url.
+        # Arguments:
+        #       None
+        # Return:
+        #       URL (future) : a future containing the result of the url shortening
+
+        return ''.join([random.choice(self.random_base) for _ in xrange(self.length_url)])
---
+++ Tornado-Application/random_url_generator/random_url_settings.py
@@ -0,0 +1,13 @@
+# random_url_settings.py include settings for the random_url_generator.py
+
+# random_base are numbers that we randomly choose when we are building an URL.
+# If there are 16 different numbers, and a 6 digit long shortened URLs, then we can build
+# 16 million different URLs
+random_base = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+               "A", "B", "C", "D", "E", "F"]
+
+# domain_base is a url that we will join with a six digit hash of the a URL to be shortened
+domain_base = "http://mxlei01.com/url-shortener"
+
+# the length of the generated shortened URL digits/letters
+length_url = 6
